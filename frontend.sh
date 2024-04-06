4208e02dc0a4e334bca274ab6e0335ff925dcc4c
Venkat R <venkatr8386@gmail.com>
2024-04-06 16:12:44 +0530

    Roboshop Nginix script

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,18 @@
-echo -e "\e[33Installing Nginix Server\e[0m"
+echo -e "\e[33mInstalling Nginix Server\e[0m"
 dnf install nginx -y
 
-echo -e "\e[33Removing Nginix html content\e[0m"
+echo -e "\e[33mRemoving Nginix html content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[Downloading frondend content\e[0m"
+echo -e "\e[33mDownloading frondend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[33Extract frondend content\e[0m"
+echo -e "\e[33mExtract frondend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 ## we need to copy the config file
 
-echo -e "\e[33Sarting Nginix Server\e[0m"
+echo -e "\e[33mSarting Nginix Server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
